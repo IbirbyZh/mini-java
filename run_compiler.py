6ea4340f8bf29e1dba769025e999c5c724ce3f8c
Шишкин Евгений Андреевич <040796@gmail.com>
2017-10-14 15:23:21 +0300

    add rebuild to python

--- run_compiler.py
+++ run_compiler.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import os
 
 
 def parse_args():
@@ -11,5 +12,20 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
+    try:
+        with open(os.devnull, "w") as nothing:
+            subprocess.check_call(
+                [
+                    '/Applications/CLion.app/Contents/bin/cmake/bin/cmake',
+                    '--build', '/Users/ibirby/Documents/mini-java/cmake-build-debug',
+                    '--target', 'all',
+                    '--', '-j', '2'
+                ],
+                stdout=nothing,
+                stderr=nothing
+            )
+    except subprocess.CalledProcessError:
+        print 'Build Failed'
+
     with open(args.input) as input_stream:
         subprocess.call([args.executable], stdin=input_stream)
